1095ec28fac7b9bbf0a717ad32a985a9da2d60c0
Rob J Goedman <goedman@mac.com>
2020-03-23 14:55:01 -0400

    Worked on ex.1.3.10

--- examples/chapters/01/ex.1.3.10a.jl
+++ examples/chapters/01/ex.1.3.10a.jl
@@ -1,16 +1,14 @@
+# Assumes `ex.1.3.10.jl` has been executed.
+
 using StructuralCausalModels, StatisticalRethinking, StatsPlots
 
 ProjDir = @__DIR__
 
-scm_01 = Dict(
-  :Y => [:X1, :X2]
-)
-
 include("$ProjDir/m.1.3.10.jl")
 
-# Use 1000 observations
+# Use 10 observations
 
-N = 1000
+N = 10
 df = DataFrame(
   :x1 => round.(rand(Uniform(1, 6), N), digits=0),
   :x2 => round.(rand(Uniform(1, 6), N), digits=0)
@@ -48,8 +46,8 @@ if success(rc)
   bnds_quantile = [quantile(mu_r[i], [0.055, 0.945]) for i in 1:length(xi)]
   bnds_hpd = [hpdi(mu_r[i], alpha=0.11) for i in 1:length(xi)];
   
-  title = "y vs. x2 ( N=1000 )" * "\nshowing sample and hpd range"
-  p1 = plot(xlab="x2", ylab="y",
+  title = "y vs. x2 ( N=10 )" * "\nshowing sample and hpd range"
+  p2 = plot(xlab="x2", ylab="y",
     title=title)
 
   x_r = rescale(xi, xbar, xstd)
@@ -65,14 +63,9 @@ if success(rc)
 
   plot!(x_r , mu_means_r, color=:black)
   scatter!(df[:, :x2], df[:, :y], leg=false, color=:darkblue)
+  plot(p1, p2, layout=(2,1))
+  
+  savefig("$ProjDir/Fig-1-13-10a.png")
 
-  savefig("$ProjDir/Fig-1-13-10.png")
-
-  # x1 and x2 are independent:
-  ucor = round(cor(df.x1, df.x2), digits=3)
-  println("\nUnconditioned correlation: cor(df.x1, df.x2) = $(ucor)\n")
-  # but conditiona on df.y
-  dfc = df[df.y .== 9, [:x1, :x2]]
-  println("Conditioned correlation, e.g. y==9: cor(dfc.x1, dfc.x2) = $(cor(dfc.x1, dfc.x2))\n")
 
 end
---
+++ examples/chapters/01/m.1.3.10.jl
@@ -0,0 +1,30 @@
+using StructuralCausalModels, StatisticalRethinking, StatsPlots
+
+ProjDir = @__DIR__
+
+ex_1_3_10 = "
+data {
+  int N;
+  real x2[N];
+  real y[N];
+}
+parameters {
+  real alpha;
+  real beta;
+  real <lower=0.0> sigma;
+}
+model {
+  vector[N] mu;
+  sigma ~ uniform( 0 , 2 );
+  alpha ~ normal(0, 2);
+  beta ~ normal( 0, 2 );
+  for (i in 1:N) {
+    mu[i] = alpha + beta * x2[i];
+  }
+  y ~ normal( mu, sigma );
+}
+";
+
+# Define the Stanmodel.
+tmpdir = joinpath(ProjDir, "tmp")
+ex_1_3_10s = SampleModel("ex_1_3_10", ex_1_3_10, tmpdir=tmpdir);
