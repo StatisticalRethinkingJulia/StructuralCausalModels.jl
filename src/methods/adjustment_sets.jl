b7004cfc63bdf0f030d8966fa9f63843e5051492
Rob J Goedman <goedman@mac.com>
2020-05-13 10:29:38 -0400

    Rel 0.1.0 - Adjustment_sets updated

--- src/methods/adjustment_sets.jl
+++ src/methods/adjustment_sets.jl
@@ -6,6 +6,31 @@ function forward_path(d::DAG, path)
   res
 end
 
+function blocking_sets(asets::Array{Array{Symbol,1},1})
+  result_sets = Vector{Symbol}[]
+  syms = union(asets...)
+  reduced_syms = deepcopy(syms)
+  for sym in syms
+    local res = true
+    for i in 1:length(asets)
+      if sym in asets[i] 
+        res = true
+      else
+        res = false
+      end
+      #println("sym = $sym, res = $res")
+      !res && break
+    end
+    if res
+      push!(result_sets, [sym])
+      setdiff!(reduced_syms, [sym])
+    end
+  end
+  #println("reduced_syms = $reduced_syms")
+  length(reduced_syms) > 0 && push!(result_sets, reduced_syms)
+  result_sets
+end
+
 function adjustment_sets(d::DAG, f::Symbol, l::Symbol, u::Vector{Symbol})
   asets = Vector{Symbol}[]
   allpaths  = all_paths(d, f, l)
@@ -19,7 +44,7 @@ function adjustment_sets(d::DAG, f::Symbol, l::Symbol, u::Vector{Symbol})
     setdiff!(aset, [path[end]])
     push!(asets, aset)
   end
-  asets
+  blocking_sets(asets)
 end
 
 function adjustment_sets(d::DAG, f::Symbol, l::Symbol)
@@ -34,7 +59,7 @@ function adjustment_sets(d::DAG, f::Symbol, l::Symbol)
     setdiff!(aset, [path[end]])
     push!(asets, aset)
   end
-  asets
+  blocking_sets(asets)
 end
 
 export
--- test/test_02.jl
+++ test/test_02.jl
@@ -21,9 +21,30 @@ d = OrderedDict(
   :m => [:d],
   :w => [:d]
 );
+u = []
 
 dag = DAG("sr6.4.3", d, df);
-#show(dag)
+show(dag)
 
-allpaths = all_paths(dag, :w, :d)
+allpaths  = all_paths(dag, :w, :d)
+println("All paths between :x and :y:")
 allpaths |> display
+println()
+
+backdoorpaths = backdoor_paths(dag, allpaths, :w)
+println("All backdoors between :w and :d:")
+backdoorpaths |> display
+println()
+
+openpaths = open_paths(dag, backdoorpaths)
+println("All open (backdoor) paths between :w and :d:")
+openpaths |> display
+println()
+
+println("Show path: $(allpaths[2])")
+show_dag_path(dag, allpaths[2]) |> display
+println()
+
+adjustmentsets = adjustment_sets(dag, :w, :d)
+println("Adjustment sets for open paths: $(openpaths)\n")
+adjustmentsets |> display
