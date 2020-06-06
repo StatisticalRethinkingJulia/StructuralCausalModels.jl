b08d94017a9bb71b5c1a31c35cd6abb6ed05e8f8
Rob J Goedman <goedman@mac.com>
2020-06-06 12:32:40 -0600

    Rel 0.1.0

---
+++ test/test_graphviz_conversions.jl
@@ -0,0 +1,45 @@
+
+
+using StructuralCausalModels
+
+ProjDir = @__DIR__
+cd(ProjDir) #do
+
+df = CSV.read(scm_path("..", "data", "marks.csv"));
+
+#=
+R_dag = "
+           mechanics vectors algebra statistics analysis
+mechanics          0       0       0          0        0
+vectors            1       0       0          0        0
+algebra            1       1       0          1        1
+statistics         0       0       0          0        0
+analysis           0       0       0          1        0
+";
+=#
+
+d = OrderedDict(
+  :mechanics => [:vectors, :algebra],
+  :vectors => [:algebra],
+  :analysis => [:algebra],
+  :statistics => [:algebra, :analysis]
+);
+
+dag = DAG("marks", d, df);
+show(dag)
+
+fn = "marks.dot"
+to_graphviz(dag, fn)
+Sys.isapple() && run(`open -a GraphViz.app $(fn)`)
+
+#=
+digraph marks {
+  "algebra" [shape="box", fillcolor="gray85", style="filled"];
+  "algebra" -> "analysis";
+  "algebra" -> "mechanics";
+  "algebra" -> "statistics";
+  "algebra" -> "vectors";
+  "analysis" -> "statistics";
+  "vectors" -> "mechanics";
+}
+=#
